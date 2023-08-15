4ea802692921c5e8a82c3fcd3ba38c38dea652e4
CHARITY-WACHIRA <kouwywachira@gmail.com>
2023-08-15 21:40:12 +0300

    completed assignment

--- lib/an_assertion_error.py
+++ lib/an_assertion_error.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 
-assert(1 == 2)
+def _get_code_from_file(fname):
+    # Check for a compiled file first
+    with open(fname, "rb") as f:
+        code = read_code(f)
+    if code is None:
+        # That didn't work, so try it as normal source code
+        with open(fname, "rU") as f:
+            code = compile(f.read(), fname, 'exec')
+            assert 1 == 2, "custom error message: 1 is not equal to 2"
